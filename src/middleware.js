2f6a7b293a394cd99f36c403a9ade7a380e88cce
Emanual Joyal <emanualjoyalka@gmail.com>
2026-02-13 00:56:24 +0530

    Started doing pages

---
+++ src/middleware.js
@@ -0,0 +1,53 @@
+import { createServerClient } from "@supabase/ssr"
+import { NextResponse } from "next/server"
+
+export async function middleware(request) {
+  let response = NextResponse.next()
+
+  
+
+
+  const supabase = createServerClient(
+    
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        setAll(cookiesToSet) {
+             cookiesToSet.forEach((cookie) => {
+                const name = cookie.name
+                const value = cookie.value
+                const options = cookie.options
+
+            response.cookies.set(name, value, options)
+         
+        })
+        },
+      },
+    }
+  )
+
+  const res = await supabase.auth.getUser()
+  const user = res?.data?.user
+
+  const pathname = request.nextUrl.pathname
+
+  // Protect dashboard
+  if (pathname.startsWith("/dashboard") && !user) {
+    return NextResponse.redirect(new URL("/", request.url))
+  }
+
+  // Redirect logged-in users away from home
+  if (pathname === "/" && user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url))
+  }
+
+  return response
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/"],
+}
